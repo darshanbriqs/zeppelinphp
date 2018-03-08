f289ef34426cee4e62fdd4b83314c22ccd29074d
Darshan Sanandiya <darshan.sanandiya@qdata.io>
2018-03-08 11:53:22 +0530

    add more paragraph operations in library

--- src/Service/ParagraphService.php
+++ src/Service/ParagraphService.php
@@ -31,4 +31,39 @@ class ParagraphService extends AbstractService
     public function statusParagraph( $note_id, $paragraph_id ) {
         return $this->client->request( 'api/notebook/job/'.$note_id.'/'.$paragraph_id.'', 'get' );   
     }
+    
+    /**
+    *  Delete a paragraph  
+    */
+    public function delete( $note_id, $paragraph_id ) {
+        return $this->client->request( 'api/notebook/'.$note_id.'/paragraph/'.$paragraph_id.'', 'delete' );   
+    }
+    
+    /**
+    *  Run a paragraph asynchronously  
+    */
+    public function runParagraphAsync( $note_id, $paragraph_id ) {
+        return $this->client->request( 'api/notebook/job/'.$note_id.'/'.$paragraph_id.'', 'post' );   
+    }
+    
+    /**
+    *  Run a paragraph synchronously  
+    */
+    public function runParagraphSync( $note_id, $paragraph_id ) {
+        return $this->client->request( 'api/notebook/run/'.$note_id.'/'.$paragraph_id.'', 'post' );   
+    }
+    
+    /**
+    *  Stop a paragraph  
+    */
+    public function stopParagraph( $note_id, $paragraph_id ) {
+        return $this->client->request( 'api/notebook/job/'.$note_id.'/'.$paragraph_id.'', 'delete' );   
+    }
+    
+    /**
+    *  Move a paragraph to the specific index 
+    */
+    public function moveParagraphToIndex( $note_id, $paragraph_id, $index ) {
+        return $this->client->request( 'api/notebook/'.$note_id.'/paragraph/'.$paragraph_id.'/move/'.$index.'', 'post' );   
+    }
 }
